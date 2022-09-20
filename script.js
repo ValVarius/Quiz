1c4688b11bbc01aa7a1fd3d1b41671b9b57b1330
ValVarius <60993964+ValVarius@users.noreply.github.com>
2022-09-20 17:43:49 -0600

    added most functionality and css

--- script.js
+++ script.js
@@ -1,2 +1,111 @@
+let quiz = [
+  {
+    q: "Inside which HTML element do we put the JavaScript?",
+    a1: "<script></script>",
+    a2: `<javascript></javascript>`,
+    a3: `<scripting></scripting>`,
+    a4: `<js></js>`,
+    correct: "1",
+  },
+  {
+    q: `What is the correct JavaScript syntax to change the content of the HTML element below? \n <p id="demo">This is a demonstration.</p>`,
+    a1: `#demo.innerHTML = "Hello World!";`,
+    a2: ` document.getElementById("demo").innerHTML = "Hello World!";`,
+    a3: ` document.getElementById("demo").innerHTML = "Hello World!";`,
+    a4: ` document.getElementByName("p").innerHTML = "Hello World!";`,
+    correct: "2",
+  },
+  {
+    q: `Where is the correct place to insert a JavaScript?`,
+    a1: `The <head> section`,
+    a2: `Both the <head> section and the <body> section are correct`,
+    a3: `The <body> section`,
+    a4: `Outside the <head> and <body> section`,
+    correct: "2",
+  },
+  {
+    q: `What is the correct syntax for referring to an external script called "xxx.js"?`,
+    a1: `<script name="xxx.js">`,
+    a2: `<script href="xxx.js">`,
+    a3: `<script src="xxx.js">`,
+    a4: `<script id="xxx.js">`,
+    correct: "3",
+  },
+];
 
+let user = "";
+let olderScore = 0;
+let currentScore = 0;
+let questionNumber = 0;
 
+const startButton = document.querySelector("#Start");
+const questionaire = document.getElementById("questionaire");
+const nameInput = document.getElementById("name-input");
+const entername = document.querySelector("#entername");
+const textname = document.querySelector("#textname");
+const OldScore = document.querySelector("#OldScore");
+const CurrentScore = document.querySelector("#CurrentScore");
+const options = document.querySelectorAll(".option");
+
+const question = document.getElementById("question");
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const option3 = document.getElementById("option3");
+const option4 = document.getElementById("option4");
+
+console.log(startButton.textContent);
+
+function getName() {
+  startButton.textContent = "Good Luck!";
+  nameInput.style.visibility = "visible";
+}
+
+function begin() {
+  user = textname.value;
+
+  // check if name exist in local storage and if yes display older score
+
+  nameInput.style.visibility = "hidden";
+
+  function wait() {
+    questionaire.style.visibility = "visible";
+
+    nextQuestion();
+  }
+  setTimeout(wait, 500);
+}
+
+function nextQuestion() {
+  if (questionNumber < quiz.length) {
+    question.textContent = quiz[questionNumber].q;
+
+    option1.textContent = quiz[questionNumber].a1;
+    option2.textContent = quiz[questionNumber].a2;
+    option3.textContent = quiz[questionNumber].a3;
+    option4.textContent = quiz[questionNumber].a4;
+  } else {
+    console.log("End of test");
+    questionaire.style.visibility = "hidden";
+    console.log(user + " has a score of " + currentScore);
+    // Store name if user is new or update score if user already exists
+    // reset all
+  }
+}
+
+function register(event) {
+  console.log(event.target.id.slice(-1), "  ", quiz[questionNumber].correct);
+
+  if (event.target.id.slice(-1) == quiz[questionNumber].correct) {
+    currentScore = currentScore + 10;
+  }
+
+  questionNumber++;
+  CurrentScore.textContent = "Current Score: " + currentScore;
+  nextQuestion();
+}
+
+startButton.addEventListener("click", getName);
+entername.addEventListener("click", begin);
+options.forEach((item) => {
+  item.addEventListener("click", register);
+});
